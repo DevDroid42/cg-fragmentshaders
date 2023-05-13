f5f672a19604c43088e0111a5f8dda379e6fc9d6
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-05-13 18:17:20 -0500

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'cg-fragmentshaders',
+  base: '/cg-fragmentshaders/',
   plugins: [vue()],
   resolve: {
     alias: {
